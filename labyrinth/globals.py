8d45dc93a1f11bd0412820435564411ab2fa586a
Gabriel Caê C Bounous <gbounous@motorola.com>
2016-08-15 11:09:52 -0300

    absolute paths instead of relative path

--- labyrinth/globals.py
+++ labyrinth/globals.py
@@ -1,7 +1,8 @@
 # -*-coding:Utf-8 -*#
+import os
 
-DB_PATH = "./dbtext.db"
-MAPS_DIR = "./maps/"
-MAPS_DEFAULT = "{}{}".format(MAPS_DIR,"default/")
-MAPS_PERSONAL = "{}{}".format(MAPS_DIR,"personal/")
-MAPS_COMPONENTS = "{}{}".format(MAPS_DIR,"personal/")
+DB_PATH = "{}/dbtext.db".format(os.getcwd())
+MAPS_DIR = "{}/maps".format(os.getcwd())
+MAPS_DEFAULT = "{}/{}".format(MAPS_DIR,"default")
+MAPS_PERSONAL = "{}/{}".format(MAPS_DIR,"personal")
+MAPS_COMPONENTS = "{}/{}".format(MAPS_DIR,"personal")
