943eb11929ee6faf5593cfa098c52fe3b5d36946
Gabriel Caê Bounous <gccbounous@gmail.com>
2016-08-16 12:34:22 -0300

    text database all tested

--- db/dbtext.py
+++ db/dbtext.py
@@ -13,56 +13,21 @@ class DBText():
         users : { user_name: {map_name : [map_objects] } }  *with robot
     """
 
-    teste = globals_.DB_SQL_PATH
-
     def __init__(self):
         """
             Constructor that initializes the data-base object
         """
         self.maps = {}
         self.users = {}
-
-    def _load_default_maps(self):
-        """
-            Method that gets the maps in the default_maps folder
-        """
-        for f_name in os.listdir(globals_.MAPS_DEFAULT):
-            with open("{}{}".format(globals_.MAPS_DEFAULT,f_name), 'r') as file_:
-                self.maps[f_name] = file_.read()
-
-    def _load_personal_maps(self):
-        """
-            Method that gets the maps in the personnal_maps folder
-        """
-        for f_name in os.listdir(globals_.MAPS_PERSONAL):
-            with open("{}{}".format(globals_.MAPS_PERSONAL,f_name), 'r') as file_:
-                self.maps[f_name] = file_.read()
-
-    def _write_db(self):
-        """
-            Method that refreshes the data base
-        """
-        with open(globals_.DB_TEXT_PATH, 'wb') as file_:
-            my_pickler = pickle.Pickler(file_)
-            my_pickler.dump(self)
-
-    def _load_db(self):
-        """
-            Method that loads the data base information into the dsata base object
-        """
-        with open(globals_.DB_TEXT_PATH, 'rb') as file_:
-            my_unpickler = pickle.Unpickler(file_)
-            loaded_db = my_unpickler.load()
-            self.maps = loaded_db.maps
-            self.users = loaded_db.users
  
-    def initialize_db(self):
+    def initialize(self):
         """
             Method that populates the object attributes
         """
         if os.path.isfile(globals_.DB_TEXT_PATH):
             self._load_db()
         else:
+            print("creating")
             self._load_default_maps()
             self._load_personal_maps()
             self._write_db()
@@ -90,7 +55,7 @@ class DBText():
         self._load_db()
         try:
             return self.maps[map_name]
-        except IndexError:
+        except KeyError:
             print "Map '{}' doesn't exist.".format(map_name)
             return None
 
@@ -103,7 +68,7 @@ class DBText():
         self._load_db()
         try:
             return self.users[user_name]
-        except IndexError:
+        except KeyError:
             print "User-id '{}' doesn't exist.".format(user_name)
             return None
 
@@ -126,7 +91,7 @@ class DBText():
             print "User-id '{}' doesn't exist.".format(user_name)
         return game_text
 
-    def set_new_user(self, user_name):
+    def new_user(self, user_name):
         """
             Method that saves a new user
             param:
@@ -141,7 +106,7 @@ class DBText():
             print "User-id '{}' already exists.".format(user_name)
             return False
 
-    def set_new_map(self, map_name, map_text):
+    def new_map(self, map_name, map_text):
         """
             Method that saves a new map
             params:
@@ -152,8 +117,9 @@ class DBText():
         if map_name not in self.maps.keys():
             if not os.path.isdir(globals_.MAPS_PERSONAL):
                 os.mkdir(globals_.MAPS_PERSONAL)
-            with open("{}{}".format(globals_.MAPS_PERSONAL,map_name), w) as file_:
-                file.write(map_text)
+            map_file = "{}.txt".format(map_name)
+            with open("{}{}".format(globals_.MAPS_PERSONAL,map_file), 'w') as file_:
+                file_.write(map_text)
             self.maps[map_name] = map_text
             self._write_db()
             return True
@@ -171,10 +137,44 @@ class DBText():
         """
         if user_name in self.users.keys():
             self.users[user_name][game_name] = game_text
+            self._write_db()
         else:
             print "User-id '{}' doesn't exist.".format(user_name)
 
-if __name__ == '__main__':
-    db = DBText()
+    ####  private functions ###
+    def _load_default_maps(self):
+        """
+            Method that gets the maps in the default_maps folder
+        """
+        for f_name in os.listdir(globals_.MAPS_DEFAULT):
+            with open("{}{}".format(globals_.MAPS_DEFAULT,f_name), 'r') as file_:
+                self.maps[f_name.split(".")[0]] = file_.read()
 
-    print globals_.DB_TEXT_PATH
+    def _load_personal_maps(self):
+        """
+            Method that gets the maps in the personnal_maps folder
+        """
+        for f_name in os.listdir(globals_.MAPS_PERSONAL):
+            with open("{}{}".format(globals_.MAPS_PERSONAL,f_name), 'r') as file_:
+                self.maps[f_name.split(".")[0]] = file_.read()
+
+    def _write_db(self):
+        """
+            Method that refreshes the data base
+        """
+        with open(globals_.DB_TEXT_PATH, 'wb') as file_:
+            my_pickler = pickle.Pickler(file_)
+            my_pickler.dump(self)
+
+    def _load_db(self):
+        """
+            Method that loads the data base information into the dsata base object
+        """
+        with open(globals_.DB_TEXT_PATH, 'rb') as file_:
+            my_unpickler = pickle.Unpickler(file_)
+            loaded_db = my_unpickler.load()
+            self.maps = loaded_db.maps
+            self.users = loaded_db.users
+
+if __name__ == '__main__':
+    print "test with labyrinth/test.py to reduce import problems"
